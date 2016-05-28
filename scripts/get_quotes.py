7e3b794e3f7ae20c8598da249e10ef6c572e27e8
Kumar Ayush <cheekujodhpur@gmail.com>
2016-05-28 18:02:51 +0530

    sending request

--- scripts/get_quotes.py
+++ scripts/get_quotes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from mechanize import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 # set up mechanize header
 headers = [('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:14.0) Gecko/20100101 Firefox/14.0.1')]
@@ -10,12 +10,26 @@ headers = [('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:14.0) Gecko/20100101 
 url = "http://www.bseindia.com/getquote.htm"
 
 br = Browser()
+
+# browser parameters
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False) 
+
 br.addheaders = headers
 
 # make request
-response = br.open(url)
+main_page = br.open(url)
 
-print response.read()
+# select the default form
+br.select_form(nr=0)
+br.find_control(id="suggestBoxEQ").value = "CAREERP"
 
-#close response
-response.close()
+# submit form
+br.submit()
+page_response = bs(br.response().read())
+
+quote = page_response.find_all('div',{'class':'newsrheadingdiv'})
+print quote
